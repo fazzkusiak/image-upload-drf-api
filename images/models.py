63874f4d60ad738c2b22be1e33bc4ca966931a08
kubusiaq <sactarek@gmail.com>
2023-03-18 13:24:38 +0100

    added tier and user models

--- images/models.py
+++ images/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Tier(models.Model):
+    name = models.CharField(max_length=20, null=False, blank=False)
+    thumbnail_200px = models.BooleanField(default=True)
+    thumbnail_400px = models.BooleanField(default=False)
+    source_image = models.BooleanField(default=False)
+    fetch_link = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class User(AbstractUser):
+    tier = models.ForeignKey(Tier, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.username
